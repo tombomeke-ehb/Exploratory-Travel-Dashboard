3dd11143bed035ea18ac47f1a8c253ff57b8f307
Tom <tom1dekoning@gmail.com>
2026-06-01 00:21:47 +0200

    fix(server): kopieer app map naar gen/srv voor BTP deployment

--- server.js
+++ server.js
@@ -4,13 +4,23 @@ const express = require('express');
 const path = require('path');
 
 cds.on('bootstrap', (app) => {
-  // Serveer app/index.html op /
+  // In productie (gen/srv) zit index.html in ../app relatief aan server.js
+  // In development zit het in ./app
+  const appDir = path.join(__dirname, 'app');
+
   app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'app', 'index.html'));
+    const indexPath = path.join(appDir, 'index.html');
+    res.sendFile(indexPath, (err) => {
+      if (err) {
+        // Fallback: probeer relatief pad
+        res.sendFile(path.join(__dirname, '..', 'app', 'index.html'), (err2) => {
+          if (err2) res.status(404).send('index.html not found: ' + appDir);
+        });
+      }
+    });
   });
 
-  // Serveer statische bestanden uit app/
-  app.use(express.static(path.join(__dirname, 'app')));
+  app.use(express.static(appDir));
 });
 
 module.exports = cds.server;
